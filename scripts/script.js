7066f7d8c208c3dad527ba90b65b75fc049cc453
gbm-nomogram <ruiqirachelzhao@gmail.com>
2021-08-15 14:04:40 -0700

    added forms

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 
 
 
+
+
 function openCity(evt, cityName) {
   // Declare all variables
   var i, tabcontent, tablinks;
@@ -20,4 +22,8 @@ function openCity(evt, cityName) {
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
+}
+
+function calculate(){
+  document.getElementById("p6").style.display = "block";
 }
