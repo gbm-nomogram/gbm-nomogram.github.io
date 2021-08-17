2540a52a221f4d3e7b4e54ddb5c7bdee1fab8ff2
gbm-nomogram <ruiqirachelzhao@gmail.com>
2021-08-16 23:40:07 -0700

    completed 2 tabs

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,6 @@
 
 
 
-
-
 function openCity(evt, cityName) {
   // Declare all variables
   var i, tabcontent, tablinks;
@@ -24,7 +22,7 @@ function openCity(evt, cityName) {
   evt.currentTarget.className += " active";
 }
 
-function calculate(){
+function calculate_rt(){
 
 
     var age_pt = 0;
@@ -39,11 +37,14 @@ function calculate(){
     
     let age = document.getElementById("age").value;
 
-        if (age > 69 && age < 80){
-           age_pt = 5;
+        if (age < 70){
+           age_pt = 0;
+        }
+        else if (age < 80) {
+            age_pt = 5;
         }
         else {
-            age_pt = 16;
+            age_pt = 16
         }
 
     let kps = document.getElementById("kps").value;
@@ -73,6 +74,7 @@ function calculate(){
             chemo_pt = 100;
           }
 
+
     let frac = document.getElementById("frac").value;
 
         if (frac == "60/30"){
@@ -81,11 +83,13 @@ function calculate(){
         else if (frac == "30/10"){
             frac_pt = 33;
           }
+        
+
 
         total_pt = age_pt + kps_pt + resection_pt + chemo_pt + frac_pt;
 
         if (total_pt < 15){
-            prob_12 = [0.6,0.7];
+            prob_12 = [0.6,1];
           }
         else if (total_pt < 38){
             prob_12 = [0.5,0.6];
@@ -108,7 +112,6 @@ function calculate(){
         else {
             prob_12 = [0, 0];
           }
-
 
 
         if (total_pt < 8){
@@ -135,16 +138,221 @@ function calculate(){
         else{
             prob_6 = [0, 0.3];
           }
+
+
   
-  const p6 = '6 Month Survival Probabiilty: ' + prob_6[0] + ' to ' + prob_6[1];
+  const p6 = '6 Month Survival Probabiilty: ' + prob_6[0]*100+'%' + ' to ' + prob_6[1]*100 +'%';
 
-  const p12 = '12 Month Survival Probabiilty: ' + prob_12[0] + ' to ' + prob_12[1];
+  const p12 = '12 Month Survival Probabiilty: ' + prob_12[0]*100+'%' + ' to ' + prob_12[1]*100+'%';
 
-  document.getElementById("prob6").innerHTML = p6;
+  document.getElementById("prob6_rt").innerHTML = p6;
 
-  document.getElementById("prob12").innerHTML = p12;
+  document.getElementById("prob12_rt").innerHTML = p12;
 
-  let x = document.getElementById("results").style.display
+  let x = document.getElementById("results_rt").style.display;
+  
+  document.getElementById("results_rt").style.display = "block";
+
+}
+
+
+function calculate_all(){
+
+
+    var age_pt = 0;
+    var kps_pt = 0;
+    var resection_pt = 0;
+    var chemo_pt = 0;
+    var frac_pt = 0;
+    var total_pt = 0;
+    var prob_12 = [];
+    var prob_6 = [];
+
+
+
+    let kps = document.getElementById("kps").value;
+
+        if (kps == "2"){
+            kps_pt = 10;
+          }
+        else if  (kps =="1"){
+            kps_pt = 20;
+          }
+
+    let resection = document.getElementById("resection").value;
+
+  
+    let chemo = document.getElementById("chemotherapy").value;
+
+        if (chemo == "no"){
+            chemo_pt = 52;
+          }
+
+
+    let frac = document.getElementById("frac").value;
+
+
+        
+    let age = document.getElementById("age").value;
+
+        if (resection == "biopsy" && frac == "no"){
+          if (age < 70){
+            age_pt = 99;
+          }
+          else if (age < 80){
+            age_pt = 100;
+          }
+          else{
+            age_pt = 64;
+          }
+
+        }
+
+        if (resection == "biopsy" && frac == "no"){
+          if (age < 70){
+            age_pt = 99;
+          }
+          else if (age < 80){
+            age_pt = 100;
+          }
+          else{
+            age_pt = 64;
+          }
+
+        }
+
+        if (resection == "gt" && frac == "no"){
+          if (age < 70){
+            age_pt = 35;
+          }
+          else if (age < 80){
+            age_pt = 36;
+          }
+          else{
+            age_pt = 64;
+          }
+
+        }
+
+        if (resection == "st" && frac == "no"){
+          if (age < 70){
+            age_pt = 59;
+          }
+          else if (age < 80){
+            age_pt = 60;
+          }
+          else{
+            age_pt = 24;
+          }
+
+        }
+
+        if (resection == "biopsy" && frac == "yes"){
+          if (age < 70){
+            age_pt = 32;
+          }
+          else if (age < 80){
+            age_pt = 30;
+          }
+          else{
+            age_pt = 37;
+          }
+
+        }
+
+        if (resection == "gt" && frac == "yes"){
+          if (age < 70){
+            age_pt = 14;
+          }
+          else if (age < 80){
+            age_pt = 12;
+          }
+          else{
+            age_pt = 19;
+          }
+
+        }
+
+        if (resection == "st" && frac == "yes"){
+          if (age < 70){
+            age_pt = 10;
+          }
+          else if (age < 80){
+            age_pt = 8;
+          }
+          else{
+            age_pt = 15;
+          }
+
+        }
+
+        total_pt = age_pt + kps_pt + resection_pt + chemo_pt + frac_pt;
+
+        if (total_pt < 6){
+            prob_12 = [0.6,1];
+          }
+        else if (total_pt < 21){
+            prob_12 = [0.5,0.6];
+          }
+        else if (total_pt < 33){
+            prob_12 = [0.4,0.5];
+          }
+        else if  (total_pt <46){
+            prob_12 = [0.3, 0.4];
+          }
+        else if (total_pt < 59){
+            prob_12 = [0.2, 0.3];
+          }
+        else if (total_pt < 75){
+            prob_12 = [0.1, 0.2];
+          }
+        else if (total_pt > 74){
+            prob_12 = [0, 0.1];
+          }
+        else {
+            prob_12 = [0, 0];
+          }
+
+
+        if (total_pt < 21){
+            prob_6 = [0.8,1];
+          }
+        else if (total_pt < 43){
+            prob_6 = [0.7,0.8];
+          }
+        else if (total_pt < 59){
+            prob_6 = [0.6, 0.7];
+          }
+        else if (total_pt < 73){
+            prob_6 = [0.5, 0.6];
+          }
+        else if (total_pt < 86){
+            prob_6 = [0.4, 0.5];
+          }
+        else if (total_pt < 98){
+            prob_6 = [0.3, 0.4];
+          }
+        else if (total_pt < 111){
+            prob_6 = [0.2, 0.3];
+          }
+        else if (total_pt < 127){
+            prob_6 = [0.1, 0.2];
+          }
+        else {
+            prob_6 = [0, 0.1];
+          }
+
+
+  
+  const p6_all = '6 Month Survival Probabiilty: ' + prob_6[0]*100+'%' + ' to ' + prob_6[1]*100+'%';
+
+  const p12_all = '12 Month Survival Probabiilty: ' + prob_12[0]*100+'%' + ' to ' + prob_12[1]*100+'%';
+
+  document.getElementById("prob6_all").innerHTML = p6_all;
+
+  document.getElementById("prob12_all").innerHTML = p12_all;
+
+  let x = document.getElementById("results").style.display;
   
   document.getElementById("results").style.display = "block";
 
